277fcca65cdbf44dfca5a9b1a8ae759e0b537f6e
rajhuraj <raghu6092s@gmail.com>
2026-03-09 23:02:01 +0530

    Update students-data.js

--- students-data.js
+++ students-data.js
@@ -8,12 +8,11 @@
 //    photo: "rahul.jpg"   ← image file same folder mein honi chahiye
 //    photo: null          ← emoji show hoga
 // ============================================================
-
 const ALL_STUDENTS = [
-  { name: "Yogendar", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "yogendar.png", icon: "🎖️" },
-  { name: "Meenu", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "meenu.jpg", icon: "🎖️" },
-  { name: "Anushka", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "anushka.jpg", icon: "🎖️" },
-  { name: "Bhavana Pushkar", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "Bhavana Pushkar.jpg", icon: "🎖️" },
-  { name: "Gaurav", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "Gaurav.jpg", icon: "🎖️" },
-  { name: "Chandrapratap", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "Chandrapratap.jpg", icon: "🎖️" },
+  { name: "Yogendar", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "batch1/yogendar.png", icon: "🎖️" },
+  { name: "Meenu", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "batch1/meenu.jpg", icon: "🎖️" },
+  { name: "Anushka", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "batch1/anushka.jpg", icon: "🎖️" },
+  { name: "Bhavana Pushkar", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "batch1/Bhavana Pushkar.jpg", icon: "🎖️" },
+  { name: "Gaurav", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "batch1/Gaurav.jpg", icon: "🎖️" },
+  { name: "Chandrapratap", village: "Etah",      district: "Etah, UP",       post: "UP police",    year: "2026", photo: "batch1/Chandrapratap.jpg", icon: "🎖️" },
 ];
